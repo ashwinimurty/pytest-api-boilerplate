b807f7248f2a7bd1474a46e4026ad64380e528f8
ashwinimurty <35322356+ashwinimurty@users.noreply.github.com>
2021-05-17 20:18:15 -0700

    Added the tests

--- test/rm_character_test.py
+++ test/rm_character_test.py
@@ -1,20 +1,50 @@
 import pytest, json
 from src.character_model import CharacterEP
+import requests
+from requests import Request, Session
+
 
 def test_character_endpoint():
     """
         Test Purpose: Test the character endpoint returns all characters
     """
-    raise NotImplementedError
+    get_obj = CharacterEP()
+    req = get_obj.get().build_request()
+    resp = get_obj.last_get
+    out = json.loads(resp.text)
+    print("All the characters output is", json.loads(resp.text))
+    ids = []
+    for ele in out['results']:
+        ids.append(ele['id'])
+    print("ALL IDS", ids)
+    #assert based on the number of ids or the ids itself
+    assert len(ids) == 20
+
 
 def test_single_character_endpoint():
     """
        Test Purpose: Test the character endpoint returns a single character
     """
-    raise NotImplementedError
+    get_obj = CharacterEP()
+    req = get_obj.get(2).build_request()
+    resp = get_obj.last_get
+    out = json.loads(resp.text)
+    print("Output for single character is", out)
+    if out['id'] == 2:
+        print("Match")
+    assert out['id'] == 2
+
 
 def test_multiple_characters():
     """
         Test Purpose: Test the character endpoint returns multiple characters
     """
-    raise NotImplementedError
+    get_obj = CharacterEP()
+    #using the input ids 1 and 3
+    req = get_obj.get([1,3]).build_request()
+    resp = get_obj.last_get
+    out = json.loads(resp.text)
+    print("Output for multiple character is", out)
+    #print("First ID",out[0]['id'])
+    #print("Second ID",out[1]['id'])
+    assert out[0]['id'] == 1 and  out[1]['id'] == 3
